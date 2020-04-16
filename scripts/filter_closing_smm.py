c5d26d5aae4ed5ffce1222489395913c257a1adf
ingdanielguerrero <dfgm2006@gmail.com>
2020-04-17 01:53:23 +0200

    UI updates

--- scripts/filter_closing_smm.py
+++ scripts/filter_closing_smm.py
@@ -7,109 +7,106 @@ import ntpath
 import re
 from datetime import datetime
 
-def run(folder):
+#def run(folder):
+#    """This module applies the closing filter to all the 
+#    images in the input folder, using the morfeo toolbox.
+#
+#    """
 
-    IMAGES_TYPES = ('.tif')
+IMAGES_TYPES = ('.tif')
 
-    #parser = argparse.ArgumentParser()
-    #parser.add_argument('folder', type=str, 
-    #                    help='directories or images files to process')
-    #args = parser.parse_args()
+def extract_image_info(image):
 
-    #folder = args.folder
+    image_name = ntpath.basename(image)
+    match = re.search(r'\d{4}_\d{2}_\d{2}', image_name) 
+    image_date = datetime.strptime(match.group(), '%Y_%m_%d').date()
 
-    def extract_image_info(image):
+    return image_name, image_date
 
-        image_name = ntpath.basename(image)
-        match = re.search(r'\d{4}_\d{2}_\d{2}', image_name) 
-        image_date = datetime.strptime(match.group(), '%Y_%m_%d').date()
+def filter_closing(image, tmp_path):
 
-        return image_name, image_date
+    tmp_image = os.path.join(tmp_path,'tmp_closing.tif')
+    process = subprocess.run(['/usr/local/lib/orfeo/bin/otbcli_GrayScaleMorphologicalOperation', 
+                                '-in', image,
+                                '-out', tmp_image,
+                                '-structype', 'ball',
+                                '-xradius', '1',
+                                '-yradius', '1',
+                                '-filter', 'closing'
+                            ],
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE, 
+                            universal_newlines=True)
+    return process, tmp_image
 
-    def filter_closing(image, tmp_path):
+def gdal_calculation(image, tmp_image, out_path):
 
-        tmp_image = os.path.join(tmp_path,'tmp_closing.tif')
-        process = subprocess.run(['/usr/local/lib/orfeo/bin/otbcli_GrayScaleMorphologicalOperation', 
-                                    '-in', image,
-                                    '-out', tmp_image,
-                                    '-structype', 'ball',
-                                    '-xradius', '1',
-                                    '-yradius', '1',
-                                    '-filter', 'closing'
-                                ],
-                                stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE, 
-                                universal_newlines=True)
-        return process, tmp_image
+    image_name = extract_image_info(image)[0]
+    closed_image = os.path.join(out_path, 'close_'+image_name)
 
-    def gdal_calculation(image, tmp_image, out_path):
+    process = subprocess.run(['gdal_calc.py',
+                                '-A', image,
+                                '-B', tmp_image,
+                                '--co=COMPRESS=LZW',
+                                '--type=UInt16',
+                                '--overwrite',
+                                '--outfile='+closed_image,
+                                '--calc=B*(A==0)+A'
+                            ],
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE, 
+                            universal_newlines=True)
+    return process
 
-        image_name = extract_image_info(image)[0]
-        closed_image = os.path.join(out_path, 'close_'+image_name)
+def remove_tmp_image(tmp_image):
 
-        process = subprocess.run(['gdal_calc.py',
-                                    '-A', image,
-                                    '-B', tmp_image,
-                                    '--co=COMPRESS=LZW',
-                                    '--type=UInt16',
-                                    '--overwrite',
-                                    '--outfile='+closed_image,
-                                    '--calc=B*(A==0)+A'
-                                ],
-                                stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE, 
-                                universal_newlines=True)
-        return process
+    os.remove(tmp_image)
+    return
 
-    def remove_tmp_image(tmp_image):
+def raw_to_processed(image):
 
-        os.remove(tmp_image)
-        return
+    image_name = extract_image_info(image)[0]
+    out_close_path = os.path.dirname(os.path.abspath(image)) \
+                    .replace('raw', '1_processed')
 
-    def raw_to_processed(image):
+    if not os.path.exists(out_close_path):
+        os.makedirs(out_close_path)
 
-        image_name = extract_image_info(image)[0]
-        out_close_path = os.path.dirname(os.path.abspath(image)) \
-                        .replace('raw', '1_processed')
+    if not os.path.exists(os.path.join(out_close_path, 'close_'+image_name)):
+        filter_process, tmp_image = filter_closing(image, out_close_path)
 
-        if not os.path.exists(out_close_path):
-            os.makedirs(out_close_path)
+        if filter_process.returncode == 0:
+            gdal_process = gdal_calculation(image, tmp_image, out_close_path)
 
-        if not os.path.exists(os.path.join(out_close_path, 'close_'+image_name)):
-            filter_process, tmp_image = filter_closing(image, out_close_path)
-
-            if filter_process.returncode == 0:
-                gdal_process = gdal_calculation(image, tmp_image, out_close_path)
-
-                if gdal_process.returncode == 0:
-                    print(f'Image: {image_name} processed.')
-                    remove_tmp_image(tmp_image)
-                else:    
-                    print(gdal_process.stderr)
-            else:
-                print("The process couldn't be completed")
-                print(filter_process.stderr)
+            if gdal_process.returncode == 0:
+                print(f'Image: {image_name} processed.')
+                remove_tmp_image(tmp_image)
+            else:    
+                print(gdal_process.stderr)
         else:
-            print(f'Skipping: Image "{image_name}" already exists')
-        return 
+            print("The process couldn't be completed")
+            print(filter_process.stderr)
+    else:
+        print(f'Skipping: Image "{image_name}" already exists')
+    return 
 
 
     # Search all Image files in inputs recursively if the files are in directories
 
-    image_files = []    
+#    image_files = []    
+#
+#    if os.path.isdir(folder):
+#        for root, dirs, files in os.walk(folder):
+#            if len(files) != 0:
+#                files = [os.path.join(root, x) for x in files if x.endswith(IMAGES_TYPES)]
+#                [image_files.append(os.path.abspath(file)) for file in files]
+#    else:
+#        print(f'ERROR: The {folder} joined is not a directory path.')
+#    print(f'There are {len(image_files)} images to process, please wait...')
+#
+#    dates = []
+#    for image in image_files:
+#        dates.append(extract_image_info(image))
+#        raw_to_processed(image)
 
-    if os.path.isdir(folder):
-        for root, dirs, files in os.walk(folder):
-            if len(files) != 0:
-                files = [os.path.join(root, x) for x in files if x.endswith(IMAGES_TYPES)]
-                [image_files.append(os.path.abspath(file)) for file in files]
-    else:
-        print(f'ERROR: The {folder} joined is not a directory path.')
-    print(f'There are {len(image_files)} images to process, please wait...')
-
-    dates = []
-    for image in image_files:
-        dates.append(extract_image_info(image))
-        raw_to_processed(image)
-
-    print('Done!')
+#    print('Done!')
