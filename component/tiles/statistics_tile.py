fc1af491d764e9f5209c1fefa8e0e40079c6da9c
dfguerrerom <dfgm2006@gmail.com>
2021-10-15 17:56:20 +0000

    refactor: adapt to new FolderSelectorTile and fill dainamically years/months when folders are selected

--- component/tiles/statistics_tile.py
+++ component/tiles/statistics_tile.py
@@ -1,5 +1,7 @@
 from os import cpu_count
 from pathlib import Path
+import re
+import datetime
 
 from traitlets import link
 from ipywidgets import Output
@@ -59,7 +61,9 @@ class StatisticsView(v.Layout):
         self.output = Output()
         self.date_selector = cw.DateSelector(season=True, remove_method=["single"])
 
-        self.w_selector = cw.PathSelector(param.PROCESSED_DIR)
+        self.w_selector_view = cw.FolderSelectorView(folder=param.PROCESSED_DIR.parent, max_depth=0)
+        
+        self.w_selector = self.w_selector_view.w_selector
 
         self.w_stats = v.Select(
             label="Statistic",
@@ -98,13 +102,22 @@ class StatisticsView(v.Layout):
         )
 
         self.children = [
-            v.Subheader(children=["Area selection"]),
-            v.Divider(),
-            self.w_selector,
-            v.Subheader(children=["Date selection"]),
-            v.Divider(),
-            self.date_selector,
-            v.Divider(),
+            v.Row(
+                children=[
+                    v.Col(
+                        children=[
+                            v.CardTitle(children=["Folder selection"]),
+                            self.w_selector_view,
+                        ]
+                    ),
+                    v.Col(
+                        children=[
+                            v.CardTitle(children=["Date selection"]),
+                            self.date_selector,
+                        ]
+                    ),
+                ]
+            ),
             self.w_stats,
             advanced_settings,
             self.btn,
@@ -118,7 +131,7 @@ class StatisticsView(v.Layout):
         link((self.w_chunk, "v_model"), (self.model, "chunks"))
 
         self.btn.on_event("click", self.on_click)
-        self.w_selector.w_field.observe(self.field_change, "v_model")
+        self.w_selector.observe(self.fill_season, "v_model")
 
     @su.loading_button()
     def on_click(self, widget, event, data):
@@ -126,60 +139,59 @@ class StatisticsView(v.Layout):
         with self.output:
             self.output.clear_output()
             stackstat.stack_composed(
-                self.w_selector, self.date_selector, self.model, self.alert
+                self.w_selector, self.date_selector, self.alert
             )
 
-    def field_change(change, date_selector, alert):
-        alert.clear()
+    def fill_season(self, change):
+        """Fill years and months when Season is Selected"""
+        
+            
+        months, years = self.get_months_years(change["new"])
+        parsed_months = self.date_selector.numbers_to_months(months)
 
-        months_and_years = get_months_years(w_selector.get_current_path(), alert)
+        self.date_selector.months_items = parsed_months
+        self.date_selector.years_items = years
 
-        if months_and_years:
-            years, months = months_and_years
-        else:
-            # Stop the excecution
-            return
-        date_selector.clear_season()
-
-        parsed_months = date_selector.numbers_to_months(months)
-        date_selector.months_items = parsed_months
-        date_selector.years_items = years
-
-    def get_months_years(self, path, alert):
+        self.date_selector.selected_months = parsed_months
+        self.date_selector.selected_years = years
+            
+    
+    def get_months_years(self, path):
 
         """From a given path of images, the function will return a list
         of the months/years present in the folder
 
         """
-
-        tifs = Path(path).glob("*.tif")
-
-        if tifs:
-            try:
-                years = list(
-                    set([ps.parse_other_files(image)[4].year for image in tifs])
-                )
-                months = list(
-                    set([ps.parse_other_files(image)[4].month for image in tifs])
-                )
-                years.sort()
-                months.sort()
-
-                return years, months
-            except Exception as e:
-                alert.add_msg(
-                    "ERROR: Check the folder and make sure that \
-                    all the images follow the format: 'YYYY_mm_dd'",
-                    type_="error",
-                )
-
-                return None
+        
+        if self.w_selector_view.w_recursive.v_model:
+            tifs = [tif for folder in path for tif in Path(folder).rglob("[!.]*.tif")]
         else:
-            alert.add_msg(
-                "ERROR: The folder is empty. Please make sure that you \
-                have run the closing filter before run the statistics.",
-                type_="error",
-            )
+            tifs = [tif for folder in path for tif in Path(folder).glob("[!.]*.tif")]
+        
+        dates = [date for date in [self.get_date(image) for image in tifs] if date]
+        
+        years = sorted(list(set(date.year for date in dates)))
+        months = sorted(list(set(date.month for date in dates)))
+        
+        return months, years
+        
+    @staticmethod
+    def get_date(image_path):
+        """
+        Parse the date of the images
+
+        Examples:
+            close_SMCmap_2019_11_09_dguerrero_1111.tif
+        """
+        filename = Path(image_path).stem
+
+        match = re.search(r'\d{4}_\d{2}_\d{2}', filename) 
+        if match:
+
+            date = datetime.datetime.strptime(match.group(), '%Y_%m_%d').date()
+            jday = date.timetuple().tm_yday
+
+            return date
 
     def get_stat_as_parameter(self):
         """Convert the selected stat as the input to the stack-composed script"""
