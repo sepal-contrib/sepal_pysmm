9ab0638e0c544d324a1071a96b94db13b06662a1
dfguerrerom <dfgm2006@gmail.com>
2021-10-15 17:55:00 +0000

    feat(FolderSelectorTile): recursively count of content

--- component/tiles/filter_tile.py
+++ component/tiles/filter_tile.py
@@ -22,24 +22,17 @@ class FilterTile(v.Card):
         
         self.output = Output()
         self.btn = sw.Btn("Apply morphological filter", class_='mb-2')
-        self.folder_alert = sw.Alert(children=["Please select a folder with images to process."]).show()
         self.alert = sw.Alert()
-        self.w_selector = cw.FolderSelector(param.RAW_DIR)
+        
+        w_selector_view = cw.FolderSelectorView(folder=param.RAW_DIR, max_depth=0)
+        self.w_selector = w_selector_view.w_selector
         
         self.children = [
             v.CardTitle(children=[sw.Markdown(cm.filter_tile.title)]),
             v.CardText(children=[sw.Markdown(cm.filter_tile.desc)]),
             v.Row(
                 children=[
-                    v.Col(children=[
-                        v.Card(
-                            children=[
-                                v.CardTitle(children=["Select a folder"]),
-                                self.folder_alert,
-                                self.w_selector
-                            ]
-                        )
-                    ]),
+                    v.Col(children=[w_selector_view,]),
                     v.Col(children=[
                         v.Card(
                             children=[
@@ -55,24 +48,6 @@ class FilterTile(v.Card):
         ]
         
         self.btn.on_event('click', self.on_click)
-        
-        self.w_selector.observe(self.get_image_number, 'v_model')
-    
-    @su.switch("loading", on_widgets=["w_selector"])
-    def get_image_number(self, change):
-        """Get the number of images in the current path list"""
-        
-        if change["new"]:
-            
-            number_of_images = sum([
-                len(list(Path(folder).glob("[!.]*.tif"))) for folder in change["new"]
-            ])
-            
-            self.folder_alert.add_msg(f"There are {number_of_images} images in the selected folder(s).")
-        else:
-            
-            self.folder_alert.add_msg(f"Please select a folder with images to process.")
-            
 
     @su.loading_button()
     def on_click(self, widget, event, data):
--- component/widget/folder_selector.py
+++ component/widget/folder_selector.py
@@ -4,30 +4,107 @@ from pathlib import Path
 from natsort import humansorted
 import sepal_ui.scripts.utils as su
 from sepal_ui.frontend.styles import *
+import sepal_ui.sepalwidgets as sw
 
-__all__ = ["FolderSelector"]
+import component.parameter as param
+
+__all__ = ["FolderSelector", "FolderSelectorView"]
+
+
+class FolderSelectorView(v.Card):
+    
+    def __init__(self, folder="/", max_depth=None, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+
+        self.alert_info = sw.Alert(
+            children=["Please select a folder with .tif images"]
+        ).show()
+        self.w_selector = FolderSelector(folder, max_depth)
+        self.w_recursive = v.Switch(label="Search recursively", v_model=False)
+
+        self.children = [
+            v.CardTitle(children=["Select a folder", v.Divider(), self.w_recursive]),
+            self.alert_info,
+            self.w_selector,
+        ]
+
+        self.w_selector.observe(self.get_image_number, "v_model")
+        self.w_recursive.observe(lambda *args: self.w_selector.reset(), 'v_model')
+
+    @su.switch("loading", "disabled", on_widgets=["w_selector"])
+    def get_image_number(self, change):
+        """Get the number of images in the current path list"""
+
+        if change["new"]:
+
+            if not self.w_recursive.v_model:
+                number_of_images = sum(
+                    [
+                        len(list(Path(folder).glob("[!.]*.tif")))
+                        for folder in change["new"]
+                    ]
+                )
+            else:
+                number_of_images = sum(
+                    [
+                        len(list(Path(folder).rglob("[!.]*.tif")))
+                        for folder in change["new"]
+                    ]
+                )
+            self.alert_info.add_msg(
+                f"There are {number_of_images} images in the selected folder(s)."
+            )
+        else:
+            self.alert_info.add_msg(f"Please select a folder with images to process.")
 
 
 class FolderSelector(v.List):
-    """ Multiple folder selector widget
-    
+    """Multiple folder selector widget
+
     Args:
         folder (str, Path) (optional): Initial folder. Default : '/'
-    
-    Params:
+        max_depth (int): maximum depth levels allowed from the initial folder.
+
+    Traits:
         v_model: Will store the selected elements.
         loading (bool): Whether the Folder Selector is loading or not.
-        
+
+    Params:
+        max_depth (int): Maximum levels of depth that can be accessed.
+        base_folder (Path): base folder, given from the folder arg.
+        max_depth_folder (Path): Maximum depth folder. Given by max_depth arg.
+        current_folder (Path): Folder in the current state of the widget.
+        folders (list): Folders in the current folder.
     """
+
     v_model = List().tag(sync=True)
     loading = Bool().tag(sync=True)
-    
-    def __init__(self, folder='/', *args, **kwargs):
+
+    def __init__(self, folder="/", max_depth=None, *args, **kwargs):
 
         self.folders = None
-        self.style_="overflow: auto; border-radius: 0 0 0 0;overflow-y:none"
-        self.max_height="340px"
-        self.min_height="300px"
+        self.max_depth = max_depth
+        self.base_folder = Path("/") if not folder else Path(folder)
+
+        if self.max_depth is not None:
+            if self.max_depth > len(self.base_folder.parents):
+                raise Exception(
+                    f"The folder {self.base_folder} only has a {len(self.base_folder.parents)} "
+                    f"depth. You've selected {self.max_depth} as max depth."
+                )
+        self.max_depth_folder = (
+            self.base_folder
+            if max_depth == 0
+            else self.base_folder.parents[max_depth - 1]
+        )
+
+        # Give an initial value. It will change over each change.
+        self.current_folder = self.base_folder
+
+        self.style_ = "overflow: auto; border-radius: 0 0 0 0;overflow-x:none"
+        self.max_height = "340px"
+        self.min_height = "300px"
 
         super().__init__(*args, **kwargs)
 
@@ -36,46 +113,52 @@ class FolderSelector(v.List):
             background_color="grey darken-3",
             color=COMPONENTS["PROGRESS_BAR"]["color"],
         )
-        
-        self.item_group = ListItemGroup(children=self.get_folder_group(folder))
-        
+
+        self.item_group = ListItemGroup(
+            children=self.get_folder_group(self.current_folder)
+        )
+
         self.children = [self.w_loading, self.item_group]
 
-        
         self.item_group.observe(self.on_folder, "v_model")
-        
-        link((self.item_group, 'selected'), (self, 'v_model'))
-        link((self.w_loading, 'indeterminate'), (self, 'loading'))
+
+        link((self.item_group, "selected"), (self, "v_model"))
+        link((self.w_loading, "indeterminate"), (self, "loading"))
 
     def get_folder_group(self, path):
-        """Get the folders in the current patn. Will be stored in folders class 
+        """Get the folders in the given path. Will be stored in folders class
         variable.
-        
+
         """
 
         folders = humansorted(
-            [
-                str(folder)
-                for folder in Path(path).glob("[!.]*/")
-                if folder.is_dir()
-            ]
+            [str(folder) for folder in Path(path).glob("[!.]*/") if folder.is_dir()]
         )
+
         self.folders = [str(Path(path).parent)] + folders
-        
+
         # Enumerate items. We will be able to know which one is the parent
         return [
-            ListItem(value=folder, position=i)
-            for i, folder in enumerate(self.folders)
+            ListItem(value=folder, position=i) for i, folder in enumerate(self.folders)
         ]
-        
+
     @su.switch("loading")
     def on_folder(self, change):
 
         if change["new"] is not None:
-            items = self.get_folder_group(self.folders[change["new"]])
-            self.item_group.children = items
-            self.item_group.v_model = None
 
+            # Check if we can access to this path or if it's restricted by depth
+            if Path(self.folders[change["new"]]) != self.max_depth_folder.parent:
+
+                self.current_folder = Path(self.folders[change["new"]])
+                items = self.get_folder_group(self.current_folder)
+                self.item_group.children = items
+                self.item_group.v_model = None
+                
+    def reset(self):
+        """Clear current selected elements"""
+        self.item_group.reset()
+        
 
 class ListItem(v.Flex):
     """Custom ListItem that will store a checkbox"""
@@ -105,26 +188,30 @@ class ListItem(v.Flex):
         else:
             icon = ICON_TYPES["DEFAULT"]["icon"]
             color = ICON_TYPES["DEFAULT"]["color"]
-            
         self.item = v.ListItem(
             children=[
                 v.ListItemAction(children=[v.Icon(color=color, children=[icon])]),
                 v.ListItemContent(
                     children=[
-                        v.ListItemTitle(children=[
-                            str(f"../{value.stem}") if position==0 else str(value.stem)
-                        ]),
+                        v.ListItemTitle(
+                            children=[
+                                str(f"../{value.stem}")
+                                if position == 0
+                                else str(value.stem)
+                            ]
+                        ),
                     ]
                 ),
             ]
         )
 
-        self.children = [self.item] if position==0 else [self.item, selected]
-            
+        self.children = [self.item] if position == 0 else [self.item, selected]
+
         if multiple:
             selected.disabled = False
             
         link((selected, "v_model"), (self, "is_selected"))
+
 
 class ListItemGroup(v.ListItemGroup):
 
@@ -137,23 +224,22 @@ class ListItemGroup(v.ListItemGroup):
         super().__init__(*args, **kwargs)
 
         self.children = children
-        
-    @observe('children')
+
+    @observe("children")
     def update_observe(self, change):
-        """Update widget observes once the childrens has changed"""
+        """Update widget observation once the childrens have changed"""
         # Reset previous selected items
         self.selected = []
-        
+
         # Observe each children with the selected trait
-        _ = [
-            item.observe(self.set_selected, "is_selected") 
-            for item
-            in self.children
-        ]
+        _ = [item.observe(self.set_selected, "is_selected") for item in self.children]
 
     def set_selected(self, change):
         """Bind every item with the selected trait if it's checkbox is checkd"""
+
+        self.selected = [(chld.value) for chld in self.children if chld.is_selected]
         
-        self.selected = [
-            (chld.value) for chld in self.children if chld.is_selected
-        ]
+    def reset(self):
+        """Clear selected elements"""
+        
+        [setattr(chld, 'is_selected', False) for chld in self.children]
